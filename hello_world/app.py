4038673429dcc96faf5df40c6c8724f4bfdee563
Glound <glound.app@gmail.com>
2026-01-30 13:06:08 +0900

    変更

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world!!!!!!!!!efaweafwe!",
+            "message": "hellohello world!!!!!!!!!efaweafwe!",
             # "location": ip.text.replace("\n", "")
         }),
     }
